41325a728dac5da76d83c8f31e5c412ede3d6531
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-24 01:46:59 -0500

    feat: set up routes and controllers

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const gamesRouter = require("./routes/gamesRouter");
+const genresRouter = require("./routes/genresRouter");
+const developersRouter = require("./routes/developersRouter");
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 dotenv.config();
+
+app.get("/", (req, res) => res.send(req.originalUrl));
+app.use("/games", gamesRouter);
+app.use("/genres", genresRouter);
+app.use("/developers", developersRouter);
 
 app.listen(process.env.APP_PORT, (error) => {
   if (error) throw error;
---
+++ routes/gamesRouter.js
@@ -0,0 +1,13 @@
+const controller = require("../controllers/gamesController");
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", controller.index);
+router.get("/:id", controller.show);
+router.get("/new", controller.new);
+router.get("/:id/edit", controller.edit);
+router.post("/new", controller.create);
+router.post("/:id", controller.update);
+router.post("/:id/delete", controller.delete);
+
+module.exports = router;
