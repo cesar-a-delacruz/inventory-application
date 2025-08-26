3e7ff71b8314d3dffa365f1536824c6f3a149581
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-25 21:58:59 -0500

    fix: reorder routes

--- classes/Router.js
+++ classes/Router.js
@@ -3,8 +3,8 @@ module.exports = class Router extends router {
   constructor(controller) {
     super();
     this.get("/", controller.index);
-    this.get("/:id", controller.show);
     this.get("/new", controller.new);
+    this.get("/:id", controller.show);
     this.get("/:id/edit", controller.edit);
     this.post("/new", controller.create);
     this.post("/:id", controller.update);
