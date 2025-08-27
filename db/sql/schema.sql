ad34f3de712fcbeba3bc2e83fe57c0c60f8f2e5f
César De La Cruz <158861368+cesar-a-delacruz@users.noreply.github.com>
2025-08-26 23:43:22 -0500

    feat: add cascade deletion for games child tables

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -20,14 +20,14 @@ CREATE TABLE games_developers (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     game_id INTEGER,
     developer_id INTEGER,
-    FOREIGN KEY (game_id) REFERENCES games(id),
-    FOREIGN KEY (developer_id) REFERENCES developers(id)
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (developer_id) REFERENCES developers(id) ON DELETE CASCADE
 );
 
 CREATE TABLE games_genres (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     game_id INTEGER,
     genre_id INTEGER,
-    FOREIGN KEY (game_id) REFERENCES games(id),
-    FOREIGN KEY (genre_id) REFERENCES genres(id)
+    FOREIGN KEY (game_id) REFERENCES games(id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genres(id) ON DELETE CASCADE
 );
